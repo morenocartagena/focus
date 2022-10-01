5c8510eb2ffb6bb5232f834f7089e060b1fce298
unknown <morenocartagena@outlook.com>
2022-10-01 00:22:13 -0600

    Commit 5 React front-end

---
+++ react-front/src/App.js
@@ -0,0 +1,12 @@
+// import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <button className='btn btn-info'>test</button>      
+    </div>
+  );
+}
+
+export default App;
